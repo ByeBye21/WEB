cbc6dfe639cb7be94d62e6836656254873d871a1
ByeBye21 <127333589+ByeBye21@users.noreply.github.com>
2023-05-19 03:52:52 +0300

    Update login.php

--- login.php
+++ login.php
@@ -24,6 +24,9 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     <title>Login</title>
     <!-- Bootstrap CSS dosyası -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    
+    <!-- Bootstrap JS -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 
     <link rel="stylesheet" href="./css/login.css">
     <link rel="stylesheet" href="./css/style.css">
@@ -33,7 +36,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <a class="navbar-brand" href="index.html">Web Teknolojileri</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
